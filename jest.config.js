0eba12dc1c6cd3a3054290342bb9da997799e219
Maximous Black <maximousblk@gmail.com>
2021-10-19 20:32:36 +0000

    update yarn lock file

--- jest.config.js
+++ jest.config.js
@@ -6,6 +6,7 @@ process.stdout.write = (str, encoding, cb) => {
   }
 };
 
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
